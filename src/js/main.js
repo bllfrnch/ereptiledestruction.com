87d0a187a05c07ba4f4003245feadc7bf131615d
Bill French <bllfrnch@gmail.com>
2017-08-23 23:47:55 -0400

    Added obfuscated email and contact info

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,18 @@
 require('picturefill');
 require('../scss/index.scss');
 
+document.addEventListener('DOMContentLoaded', function() {
+  var contact = document.querySelector('.section-contact');
+  var container = contact.querySelector('span.ea');
+
+  var h = 'hello'.split();
+  var fuckoff = '@';
+  var d ='ereptiledestruction.com'.split();
+  var ea = h.join() + fuckoff + d.join();
+
+  container.append(ea);
+});
+
 export default {
   x: {},
   y: () => {
