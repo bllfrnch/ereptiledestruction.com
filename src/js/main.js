0a6075b56513e8acd2b5daf18752594a36bc8884
Bill French <bllfrnch@gmail.com>
2017-08-24 23:06:18 -0400

    styling

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   var container = contact.querySelector('span.ea');
 
   var h = 'hello'.split();
-  var fuckoff = '@';
+  var fuckoff = '\x40';
   var d ='ereptiledestruction.com'.split();
   var ea = h.join() + fuckoff + d.join();
 
